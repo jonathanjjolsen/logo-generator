7a173eb59dd5f0ee4be6629ea9e75f5f1d962337
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-06-15 14:19:44 -0700

    Triangle and Square Tests Added

--- __tests__/shapetests.js
+++ __tests__/shapetests.js
@@ -1,10 +1,29 @@
 const {Circle,Triangle,Square} = require('../lib/shapes.js')
 
+//Tests for circle by creating a new circle and setting its color value to blue
 describe('circle', () => {
-    test('Render Circle', () => {
+    test('Generate Circle', () => {
         const shape = new Circle();
         let color =('blue');
         shape.setColor(color);
         expect(shape.render()).toEqual(`<circle cx="50%" cy ="50%" r="40%" height="100%" width="100%" fill="${color}"></circle>`)
+    })
+})
+
+describe('triangle', () => {
+    test('Generate Triangle', () => {
+        const shape = new Triangle();
+        let color =('blue');
+        shape.setColor(color);
+        expect(shape.render()).toEqual(`<polygon points="150,0 300,200 0,200" fill="${color}"></polygon>`)
+    })
+})
+
+describe('square', () => {
+    test('Generate Square', () => {
+        const shape = new Square();
+        let color =('blue');
+        shape.setColor(color);
+        expect(shape.render()).toEqual(`<rect x="50" height="200" width="200" fill="${color}"></rect>`)
     })
 })
